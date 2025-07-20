0db8bbd13598056328251afdc6e0fa5769ef3343
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-07-20 12:02:36 +0530

    Update Quiz.java

--- src/app_quiz/Quiz.java
+++ src/app_quiz/Quiz.java
@@ -1,4 +1,41 @@
 package app_quiz;
 
-public class Quiz {
+import javax.swing.*;
+import java.awt.*;
+
+public class Quiz extends JFrame {
+
+    JLabel qnoLbl,questionsLbl;
+
+    Quiz(){
+        super("Quiz");
+
+        ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("app_quiz/icons/quiz.png"));
+        Image image = icon.getImage().getScaledInstance(1440,292,Image.SCALE_DEFAULT);
+        JLabel lblImg = new JLabel(new ImageIcon(image));
+        lblImg.setBounds(0,0,1440,292);
+        add(lblImg);
+
+        qnoLbl = new JLabel();
+        qnoLbl.setBounds(100,450,50,30);
+        qnoLbl.setFont(new Font("Tahoma",Font.PLAIN,24));
+        add(qnoLbl);
+
+        questionsLbl = new JLabel();
+        questionsLbl.setBounds(150,450,50,30);
+        questionsLbl.setFont(new Font("Tahoma",Font.PLAIN,24));
+        add(questionsLbl);
+
+
+
+
+        getContentPane().setBackground(Color.WHITE);
+        setBounds(0,0,1440,850);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Quiz();
+    }
 }
